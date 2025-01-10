901b53dc65cdafbee78c9741e256bbe7fa32de56
Michael Milton <michael.r.milton@gmail.com>
2025-01-10 11:25:42 +1100

    Clean up unused functions

--- src/jelms/__init__.py
+++ src/jelms/__init__.py
@@ -5,31 +5,15 @@ from linkml.generators.jsonldcontextgen import ContextGenerator
 from linkml_runtime.linkml_model import SchemaDefinition
 from linkml_runtime import SchemaView
 from pydantic import BaseModel
-from pyld import jsonld
 from linkml_runtime.utils.formatutils import remove_empty_items
 
-def obj_hook(o: Any) -> str:
-    if isinstance(o, BaseModel):
-        # Let Pydantic handle the basic serialization
-        d = dict(o)
-
-        # Add @id to the object
-        for slot_name in self.sv.class_slots(o.__class__.__name__):
-            slot = self.sv.get_slot(slot_name)
-            if slot.identifier:
-                d["@id"] = d[slot_name]
-                del d[slot_name]
-                break
-        else:
-            raise ValueError(f"Class {model.__class__.__name__} has no identifier, so it cannot be serialized to JSON-LD")
-
-        # Add the @context to the object, if requested
-        if self.context_mode == "per-object":
-            d["@context"] = json.loads(ContextGenerator(schema=self.sv.schema).serialize())["@context"]
-
-        return self.encode(d)
-
 class JsonLdSerializer(json.JSONEncoder):
+    """
+    JSON Encoder that special-cases Pydantic models for JSON-LD serialization
+
+    Context is not handled here, because it's best added at the top level of the document, which
+    this class can't distinguish from a nested document.
+    """
     def __init__(self, *args, sv: SchemaView, context_mode: Literal["per-object", "none"], **kwargs):
         self.sv = sv
         self.context_mode = context_mode
@@ -48,7 +32,7 @@ class JsonLdSerializer(json.JSONEncoder):
                     del d[slot_name]
                     break
             else:
-                raise ValueError(f"Class {model.__class__.__name__} has no identifier, so it cannot be serialized to JSON-LD")
+                raise ValueError(f"Class {d.__class__.__name__} has no identifier, so it cannot be serialized to JSON-LD")
 
             # Add the @context to the object, if requested
             if self.context_mode == "per-object":
@@ -61,25 +45,9 @@ class JsonLdSerializer(json.JSONEncoder):
         else:
             raise TypeError(f"Object of type {type(o)} is not JSON serializable")
 
-def model_to_dict(model: BaseModel, sv: SchemaView) -> dict:
-    ret = model.dict()
-    # Add @id if the slot is an identifier
-    for slot_name in sv.class_slots(model.__class__.__name__):
-        slot = sv.get_slot(slot_name)
-        if slot.identifier:
-            ret["@id"] = ret[slot_name]
-            del ret[slot_name]
-            break
-    else:
-        raise ValueError(f"Class {model.__class__.__name__} has no identifier, so it cannot be serialized to JSON-LD")
-    
-    # Add @type if the slot is an identifier
-    ret["@type"] = model.__class__.__name__
-    return remove_empty_items(ret)
-
 def remove_id_mapping(d: Any) -> Any:
     """
-    Removes any context keys that map to @id, since we want to use the @id key only
+    Recursively removes any context keys that map to @id, since we want to use the @id key only
     """
     if isinstance(d, dict):
         return {k: remove_id_mapping(v) for k, v in d.items() if v != "@id"}
@@ -88,6 +56,11 @@ def remove_id_mapping(d: Any) -> Any:
     return d
 
 class JsonLdDumper(Dumper):
+    """
+    LinkML Dumper that serializes to idiomatic JSON-LD
+
+    Note: only works with Pydantic models.
+    """
     def dumps(self, element: BaseModel | Iterable[BaseModel], schema: str | SchemaDefinition, flatten: bool = False, **_) -> str:
         sv = SchemaView(schema)
         serializer = JsonLdSerializer(sv=sv, context_mode="none")
