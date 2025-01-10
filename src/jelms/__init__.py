74bf592d6596dc6e7c0b9ae5d2b81a63bece8b0e
Michael Milton <michael.r.milton@gmail.com>
2025-01-10 11:16:00 +1100

    JSON dumping passing tests

--- src/jelms/__init__.py
+++ src/jelms/__init__.py
@@ -1,5 +1,5 @@
 import json
-from typing import Iterable
+from typing import Iterable, Any, Literal
 from linkml_runtime.dumpers.dumper_root import Dumper
 from linkml.generators.jsonldcontextgen import ContextGenerator
 from linkml_runtime.linkml_model import SchemaDefinition
@@ -7,6 +7,59 @@ from linkml_runtime import SchemaView
 from pydantic import BaseModel
 from pyld import jsonld
 from linkml_runtime.utils.formatutils import remove_empty_items
+
+def obj_hook(o: Any) -> str:
+    if isinstance(o, BaseModel):
+        # Let Pydantic handle the basic serialization
+        d = dict(o)
+
+        # Add @id to the object
+        for slot_name in self.sv.class_slots(o.__class__.__name__):
+            slot = self.sv.get_slot(slot_name)
+            if slot.identifier:
+                d["@id"] = d[slot_name]
+                del d[slot_name]
+                break
+        else:
+            raise ValueError(f"Class {model.__class__.__name__} has no identifier, so it cannot be serialized to JSON-LD")
+
+        # Add the @context to the object, if requested
+        if self.context_mode == "per-object":
+            d["@context"] = json.loads(ContextGenerator(schema=self.sv.schema).serialize())["@context"]
+
+        return self.encode(d)
+
+class JsonLdSerializer(json.JSONEncoder):
+    def __init__(self, *args, sv: SchemaView, context_mode: Literal["per-object", "none"], **kwargs):
+        self.sv = sv
+        self.context_mode = context_mode
+        super().__init__(*args, **kwargs)
+
+    def default(self, o: Any) -> Any:
+        if isinstance(o, BaseModel):
+            # Let Pydantic handle the basic serialization
+            d = dict(o)
+
+            # Add @id to the object
+            for slot_name in self.sv.class_slots(o.__class__.__name__):
+                slot = self.sv.get_slot(slot_name)
+                if slot.identifier:
+                    d["@id"] = d[slot_name]
+                    del d[slot_name]
+                    break
+            else:
+                raise ValueError(f"Class {model.__class__.__name__} has no identifier, so it cannot be serialized to JSON-LD")
+
+            # Add the @context to the object, if requested
+            if self.context_mode == "per-object":
+                d["@context"] = json.loads(ContextGenerator(schema=self.sv.schema).serialize())["@context"]
+
+            # Add @type if the slot is an identifier
+            d["@type"] = o.__class__.__name__
+
+            return remove_empty_items(d)
+        else:
+            raise TypeError(f"Object of type {type(o)} is not JSON serializable")
 
 def model_to_dict(model: BaseModel, sv: SchemaView) -> dict:
     ret = model.dict()
@@ -24,20 +77,33 @@ def model_to_dict(model: BaseModel, sv: SchemaView) -> dict:
     ret["@type"] = model.__class__.__name__
     return remove_empty_items(ret)
 
+def remove_id_mapping(d: Any) -> Any:
+    """
+    Removes any context keys that map to @id, since we want to use the @id key only
+    """
+    if isinstance(d, dict):
+        return {k: remove_id_mapping(v) for k, v in d.items() if v != "@id"}
+    elif isinstance(d, list):
+        return [remove_id_mapping(v) for v in d]
+    return d
+
 class JsonLdDumper(Dumper):
     def dumps(self, element: BaseModel | Iterable[BaseModel], schema: str | SchemaDefinition, flatten: bool = False, **_) -> str:
         sv = SchemaView(schema)
+        serializer = JsonLdSerializer(sv=sv, context_mode="none")
         context_generator = ContextGenerator(schema=schema)
+        context = remove_id_mapping(json.loads(context_generator.serialize())["@context"])
 
         if isinstance(element, BaseModel):
-            return json.dumps(model_to_dict(element, sv))
+            # model = model_to_dict(element, sv)
+            # model["@context"] = context
+            doc = json.loads(serializer.encode(element))
+            doc["@context"] = context
         else:
             # If we are dumping multiple elements, we can use a named graph in JSON-LD
             # https://www.w3.org/TR/json-ld/#named-graphs
-            graph = {
-                "@graph": [model_to_dict(e, sv) for e in element],
-                "@context": json.loads(context_generator.serialize())["@context"]
+            doc = {
+                "@graph": [e for e in element],
+                "@context": context
             }
-            if flatten:
-                graph = jsonld.flatten(graph)
-            return json.dumps(graph)
+        return serializer.encode(doc)
--- test/test_dumper.py
+++ test/test_dumper.py
@@ -21,3 +21,24 @@ def test_unrelated_people():
     assert list(graph.objects(subject=EXAMPLE["first-person"], predicate=RDF.type)) == [SCHEMA["Person"]]
     assert list(graph.objects(subject=EXAMPLE["second-person"], predicate=SCHEMA.name)) == [Literal("Second Person")]
     assert list(graph.objects(subject=EXAMPLE["second-person"], predicate=RDF.type)) == [SCHEMA["Person"]]
+
+def test_related_people():
+    dumper = JsonLdDumper()
+    raw = dumper.dumps(Person(
+        name="First Person",
+        id="http://example.org/first-person",
+        knows=Person(
+            name="Second Person",
+            id="http://example.org/second-person"),
+        ),
+        schema="test/test_schema.yaml"
+    )
+
+    # Check that it's not only valid RDF, but that the relationships are correct
+    graph = Graph()
+    graph.parse(data=raw, format="json-ld")
+    assert list(graph.objects(subject=EXAMPLE["first-person"], predicate=SCHEMA.name)) == [Literal("First Person")]
+    assert list(graph.objects(subject=EXAMPLE["first-person"], predicate=RDF.type)) == [SCHEMA["Person"]]
+    assert list(graph.objects(subject=EXAMPLE["first-person"], predicate=SCHEMA.knows)) == [EXAMPLE["second-person"]]
+    assert list(graph.objects(subject=EXAMPLE["second-person"], predicate=SCHEMA.name)) == [Literal("Second Person")]
+    assert list(graph.objects(subject=EXAMPLE["second-person"], predicate=RDF.type)) == [SCHEMA["Person"]]
